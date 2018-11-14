2a24da62f298ed883fbbc8fb315ebafbfb53f205
Taeyeob <21300443@handong.edu>
2018-11-14 19:52:54 +0900

    fix

--- lib/fail.dart
+++ lib/fail.dart
@@ -18,7 +18,6 @@ class LoginFailPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
       body: new Stack(
         children: <Widget>[
